fef8e20c1da713b4abb3d6e3e2fd0bc78150a398
da.lozano13 <da.lozano13@ISC329.sis.virtual.uniandes.edu.co>
2015-10-07 20:59:18 -0500

    Ajuste al servicio persistencia

--- Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/entities/Usuario.java
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/entities/Usuario.java
@@ -19,6 +19,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  * Clase que representa un usuario del sistema
